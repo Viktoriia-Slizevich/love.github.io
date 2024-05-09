78419b8beb2fbae846e2b7733ccfc629bbfd040c
Viktoriia-Slizevich <167226508+Viktoriia-Slizevich@users.noreply.github.com>
2024-05-09 20:32:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+const answers_no = {
+    russian: [
+        "нет",
+        "Точно?",
+        "Ты правда уверен??",
+        "Правда правда???",
+        "Может все же передумать?",
+        "Я стираю ваши носочки вообще-то..",
+        "Почему так жестоко?",
+        "Может обсудим?",
+        "Ладно больше не буду спрашивать!",
+        "Может все же передумаешь?",
+        "Я вас люблю вообще-то!",
+        "Ну и еще разочек...:("
+    ]
+};
+
+answers_yes = {
+    "english": "Нет"
+}
+
+let language = "russian"; // Default language is English
+const no_button = document.getElementById('no-button');
+const yes_button = document.getElementById('yes-button');
+let i = 1;
+let size = 50;
+let clicks = 0;
+
+no_button.addEventListener('click', () => {
+    // Change banner source
+    let banner = document.getElementById('banner');
+    if (clicks === 0) {
+        banner.src = "public/images/no.gif";
+        refreshBanner();
+    }
+    clicks++;
+    // increase button height and width gradually to 250px
+    const sizes = [40, 50, 30, 35, 45]
+    const random = Math.floor(Math.random() * sizes.length);
+    size += sizes[random]
+    yes_button.style.height = `${size}px`;
+    yes_button.style.width = `${size}px`;
+    let total = answers_no[language].length;
+    // change button text
+    if (i < total - 1) {
+        no_button.innerHTML = answers_no[language][i];
+        i++;
+    } else if (i === total - 1) {
+        alert(answers_no[language][i]);
+        i = 1;
+        no_button.innerHTML = answers_no[language][0];
+        yes_button.innerHTML = answers_yes[language];
+        yes_button.style.height = "50px";
+        yes_button.style.width = "50px";
+        size = 50;
+    }
+});
+
+yes_button.addEventListener('click', () => {
+    // change banner gif path
+    let banner = document.getElementById('banner');
+    banner.src = "public/images/yes.gif";
+    refreshBanner();
+    // hide buttons div
+    let buttons = document.getElementsByClassName('buttons')[0];
+    buttons.style.display = "none";
+    // show message div
+    let message = document.getElementsByClassName('message')[0];
+    message.style.display = "block";
+});
+
+function refreshBanner() {
+    // Reload banner gif to force load  
+    let banner = document.getElementById('banner');
+    let src = banner.src;
+    banner.src = '';
+    banner.src = src;
+}
+
+function changeLanguage() {
+    const selectElement = document.getElementById("language-select");
+    const selectedLanguage = selectElement.value;
+    language = selectedLanguage;
+
+    // Update question heading
+    const questionHeading = document.getElementById("question-heading");
+    questionHeading.textContent = "Вы меня так же любите?;(";
+
+    // Reset yes button text
+    yes_button.innerHTML = answers_yes[language];
+
+    // Reset button text to first in the new language
+    if (clicks === 0) {
+        no_button.innerHTML = answers_no[language][0];
+    } else {
+        no_button.innerHTML = answers_no[language][clicks];
+    }
+
+    // Update success message
+    const successMessage = document.getElementById("success-message");
+    successMessage.textContent = "Даааа я так и знала, люблю вас :3";
+}
